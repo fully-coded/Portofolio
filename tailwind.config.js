ce54d309565e3d6cf09991a56e317c1dffd2d5bc
Jeremy Antonio <jerz.antonio10@gmail.com>
2022-02-09 22:26:45 +0800

    Section Education Finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ module.exports = {
       },
       colors: {
         transparent: "transparent",
+        pinkInfo: "#F8EFF1",
         darkblue: "#100E3C",
+        hoverbtn: "#8583E3",
       },
-      backgroundImage: {
-        imageProfil: "url('/img/Profil.jpg')",
+      spacing: {
+        54: "13.5rem",
       },
     },
   },
